3f8e3c440254a7fdb59f77eebdfaf5e83ac20aff
Marvin Emechebe <marvine@getbucks.com>
2020-04-07 22:00:50 +0200

    removed unwanted declaration

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,6 +1,3 @@
-import * as React from 'react';
-import { connect } from 'react-redux'
-
 export const cartItemsWithQuantities = (cartItems) => {
   return cartItems.reduce((acc, item) => {
     const filteredItem = acc.filter(item2 => item2.id === item.id)[0]
